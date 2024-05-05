7b3cbe0541a066757703d95b91726c9beff21f7b
Artyom Fisunenko <artyom.fisunenko@gmail.com>
2024-05-05 15:23:04 +0300

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PyCompGeomAlgorithms",
-    version="1.0.23",
+    version="1.0.24",
     author="artandfi (Artem Fisunenko)",
     author_email="artyom.fisunenko@gmail.com",
     description="An implementation of computational geometry algorithms in Python3.",
